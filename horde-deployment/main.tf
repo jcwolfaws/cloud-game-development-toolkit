0db6760067957d709fc549dc39f3109af4f42cb8
Jake Wolf <jcwolf@amazon.com>
2024-12-09 15:21:14 -0700

    FIrst big commit

---
+++ horde-deployment/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired AWS region
+}
+
+module "horde" {
+    source = "../modules/unreal/horde"
+
+    # Set values for required variables
+    github_credentials_secret_arn = var.github_credentials_secret_arn  # ARN of your GitHub credentials secret
+    certificate_arn = var.certificate_arn # ARN of the Certificate
+    unreal_horde_service_subnets = var.unreal_horde_service_subnets
+    vpc_id = var.vpc_id
+
+    # Add the fully_qualified_domain_name
+    fully_qualified_domain_name   = var.domain_name  # Replace with your desired FQDN
+
+    # Add other variables as needed based on variables.tf requirements
+
+    unreal_horde_external_alb_subnets = var.unreal_horde_external_alb_subnets  # External ALB used by developers
+    unreal_horde_internal_alb_subnets = var.unreal_horde_internal_alb_subnets # Internal ALB used by agents
+
+    agents = {
+        ubuntu-x86 = {
+            ami           = data.aws_ami.ubuntu_noble_amd.id
+            instance_type = "c7a.8xlarge"
+            min_size      = 4
+            max_size      = 10
+            block_device_mappings = [
+                {
+                    device_name = "/dev/sda1"
+                    ebs = {
+                    volume_size = 64
+                    }
+                }
+            ]
+        }
+    }
+}
---
+++ horde-deployment/variables.tf
@@ -0,0 +1,34 @@
+variable "certificate_arn" {
+  description = "ARN of the SSL/TLS certificate"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Domain name of the website"
+  type        = string
+}
+
+variable "vpc_id" {
+    description = "ID of the VPC"
+    type        = string
+}
+
+variable "unreal_horde_service_subnets" {
+    description = "IDs of the subnets"
+    type        = list(string)
+}
+
+variable "github_credentials_secret_arn" {
+    description = "ARN of the secret containing GitHub credentials"
+    type        = string
+}
+
+variable "unreal_horde_internal_alb_subnets" {
+    description = "IDs of the subnets"
+    type        = list(string)
+}
+
+variable "unreal_horde_external_alb_subnets" {
+    description = "IDs of the subnets"
+    type        = list(string)
+}
